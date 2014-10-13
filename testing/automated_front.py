adb8741203490839b68c4a6576a5540b26e7e241
Cameron Poole <Cameron.j.poole@gmail.com>
2014-10-13 21:23:49 +0800

    Added testing selenium and flask-testing

--- testing/automated_front.py
+++ testing/automated_front.py
@@ -1,11 +1,42 @@
 from selenium import webdriver
+import unittest
+import argparse
 
-hosts = #read hosts from file
-#Could have class simple for now
+hosts = [ host.strip("\n") for host in open('../logs/hosts_file','r').xreadlines() ] 
+#Just use last host from host file
+HOST = hosts[len(hosts)-1]
+LOCALHOST='http://localhost:5000/'
 
-#This requires chromedriver
-driver = webdriver.Chrome()
+class SeleniumTestCase(unittest.TestCase):
+	client = None
 
-driver.get(hosts)
+	@classmethod
+	def setUpClass(self):
+		try:
+			self.client = webdriver.Chrome()
+		except:
+			print "Failed to get webdriver. Make sure chromedriver is in path\n"
+			print "If shell is bash export PATH=$PATH:."
+			pass
 
-assert driver.title = 'Rasvamt'
+	@classmethod 
+	def tearDownClass(self): 
+		if self.client:
+		 # stop the flask server and the browser 
+		 	self.client.get(LOCALHOST+'shutdown')
+		 	self.client.close()
+	def setUp (self): 
+		if not self.client: 
+			self.skipTest('Web browser not available')
+	def tearDown(self): 
+		pass
+	def test_home_page(self):
+		self.client.get(LOCALHOST)
+		assert self.client.title = 'Rasvamt'
+
+
+if __name__ == '__main__':
+	unittest.main()
+	#This requires chromedriver
+
+
